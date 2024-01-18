964c6b0e6c1ca131a44570aea1168799bf3318d1
ngd adams <ngd.adams@gmail.com>
2024-01-18 13:29:39 +1300

    Adding first outline of js code and readme file

---
+++ mixedMessages.js
@@ -0,0 +1,24 @@
+const cardDelt = () => {
+    // create a funcction that generates a random card from a deck which is an array and each element is an object with suit and number and name 
+    // will only be playing from one deck so can't get the same card twice in the same round
+};
+
+const yourCards = () => {
+    // enter funtion here using returning two cards using cardDelt function
+};
+
+const dealerCards = () => {
+    // enter funtion here using returning two cards using cardDelt function
+};
+
+const determineWinner = () => {
+    //function that determines a winner based on the cards delt and sends a message to the console.
+    
+};
+
+//later include The dealer must continue to take cards until the total is 17 or more, at which point the dealer must stand. If the dealer has an ace, and counting it as 11 would bring the total to 17 or more (but not over 21), the dealer must count the ace as 11 and stand
+// player decides to hit if less than 14 but stands at 19
+
+console.log(`you were delt ${yourCard1} & ${yourCard2} your total is ${yourtotal1}`);
+console.log(`Blackjack!`); // wrap in an if statement for the option of 21
+console.log(`the dealer got ${dealerCard1}` & ${dealerCard2} the dealer has ${dealertotal}`);
