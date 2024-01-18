19cc1d6ac717d6d0597065b20d909948a8a5b104
ngd adams <ngd.adams@gmail.com>
2024-01-18 14:28:49 +1300

    function to deal cards to a player and dealer with no duplicates

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,16 +1,52 @@
-const cardDelt = () => {
-    // create a funcction that generates a random card from a deck which is an array and each element is an object with suit and number and name 
-    // will only be playing from one deck so can't get the same card twice in the same round
+const suits = ['spade', 'club', 'diamond', 'heart'];
+const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+// Function to generate a random suit
+const randSuit = arr => {
+    let i = Math.floor(Math.random() * arr.length);
+    return arr[i];
+}; 
+
+// Function to generate a random number
+const randRank = arr => {
+    let i = Math.floor(Math.random() * arr.length);
+    return arr[i];
+}; 
+
+// Function to generate a random card
+const generateRandomCard = (arr1, arr2) => {
+    return { suit: randSuit(arr1), rank: randRank(arr2) };
 };
 
-const yourCards = () => {
-    // enter funtion here using returning two cards using cardDelt function
+// Function to check if a card is already in the given array
+const isCardInHand = (card, array) => {
+    return array.some(item => item.suit === card.suit && item.rank === card.rank);
 };
 
-const dealerCards = () => {
-    // enter funtion here using returning two cards using cardDelt function
+// Function to deal cards to the player and dealer without duplicates
+const dealCards = () => {
+    const playerHand = [];
+    const dealerHand = [];
+
+    while (playerHand.length < 2 || dealerHand.length < 2) {
+        const newCard = generateRandomCard(suits, ranks);
+
+        if (playerHand.length < 2 && !isCardInHand(newCard, playerHand)) {
+            playerHand.push(newCard);
+        }
+
+        if (dealerHand.length < 2 && !isCardInHand(newCard, dealerHand)) {
+            dealerHand.push(newCard);
+        }
+    }
+
+    return { player: playerHand, dealer: dealerHand };
 };
 
+console.log(dealCards().player);
+console.log(dealCards().dealer);
+
+/*
 const determineWinner = () => {
     //function that determines a winner based on the cards delt and sends a message to the console.
     
@@ -22,3 +58,4 @@ const determineWinner = () => {
 console.log(`you were delt ${yourCard1} & ${yourCard2} your total is ${yourtotal1}`);
 console.log(`Blackjack!`); // wrap in an if statement for the option of 21
 console.log(`the dealer got ${dealerCard1}` & ${dealerCard2} the dealer has ${dealertotal}`);
+*/
